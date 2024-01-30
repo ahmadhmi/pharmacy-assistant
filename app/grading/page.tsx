92a6f4769e54d171ebf524e52abdb5a2735b58e4
John Dao <104119657+johnd959@users.noreply.github.com>
2024-01-30 11:11:41 -0700

    Started work on grading screen, need to clarify data storage hierarchy

---
+++ app/grading/page.tsx
@@ -0,0 +1,22 @@
+import { Student } from "@/types/student"
+import StudentAccordion from "../UI/Grading/StudentAccordion"
+
+let students:Student[] = [
+    {
+        id: "111222333444555",
+        firstName: "Jane",
+        lastName: "Doe", 
+    }
+]
+
+export default function Grading(){
+
+
+    return(
+        <main className="flex justify-center">
+            <div className="flex flex-col">
+                <StudentAccordion student={students[0]}></StudentAccordion>
+            </div>
+        </main>
+    )
+}
