ec3fed71967ed670e225c90254eed5b3648acf71
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2024-01-30 13:13:52 -0700

    added google and github authentication providers using nextAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <nav>Pharmacy Assistant</nav>;
+  return (
+    <div>
+      <nav>Pharmacy Assistant</nav>
+      <Link href="/api/auth/signin">Login</Link>
+    </div>
+  );
 }
