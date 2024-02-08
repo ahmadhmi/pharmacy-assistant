ea2ec797f2a0a0ec94658cf63b469a4b4d7aeb2e
Ahmad Heshmati <amd.heshmati@gmail.com>
2024-02-07 21:08:56 -0700

    added google provider for authentication

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,4 +1,5 @@
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "@/app/auth/mongoClient"
 import { NextAuthOptions } from "next-auth";
@@ -10,6 +11,10 @@ const authOptions: NextAuthOptions = {
       GitHubProvider({
         clientId: process.env.GITHUB_CLIENT_ID!,
         clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       }),
     ],
     session:{
