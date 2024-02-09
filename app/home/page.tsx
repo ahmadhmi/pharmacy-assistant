cbe00c94ab25f1027d45f265c02a20ea9be25205
John Dao <104119657+johnd959@users.noreply.github.com>
2024-02-09 00:25:40 -0700

    Tested dyn routing for blocks/ and added middleware to protect against unauthenticated users

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,95 +13,7 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
 
-  const {blocks} = useBlocksContext(); 
-
-  const {data:session} = useSession(); 
-
-  // const test = async () => {
-  //   try {
-  //     const blockId = "65bd6d9bc6248e23a7c07e02"; // Replace with the actual block ID
-  //     const response = await fetch(`http://localhost:3000/api/blocks/0`, {
-  //       method: "DELETE",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         block: {
-  //           id: blockId,
-  //         },
-  //       }),
-  //     });
-  
-  //     if (!response.ok) {
-  //       // Handle the error if the response status is not okay (e.g., 404 Not Found)
-  //       const errorData = await response.json();
-  //       console.error(errorData.error);
-  //     } else {
-  //       console.log("Block deleted successfully");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   }
-  // };
-  // test();
-
-  // const test1 = async () => {
-  //   try {
-  //     const blockId = "65bd6d9bc6248e23a7c07e02"; // Replace with the actual block ID
-  //     const response = await fetch(`http://localhost:3000/api/blocks/1`, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         block: {
-  //           id: blockId,
-  //         },
-  //       }),
-  //     });
-  
-  //     if (!response.ok) {
-  //       // Handle the error if the response status is not okay (e.g., 404 Not Found)
-  //       const errorData = await response.json();
-  //       console.error(errorData.error);
-  //     } else {
-  //       console.log("Block deleted successfully");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   }
-  // };
-  // test1();
-
-  // const test2 = async () => {
-  //   try {
-  //     const blockId = "65c25ddc239a2f9abed3c8aa"; // Replace with the actual block ID
-  //     const newName = "Block 10"
-  //     const response = await fetch(`http://localhost:3000/api/blocks/1`, {
-  //       method: "PATCH",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         block: {
-  //           id: blockId,
-  //           newName: newName
-  //         },
-  //       }),
-  //     });
-  
-  //     if (!response.ok) {
-  //       // Handle the error if the response status is not okay (e.g., 404 Not Found)
-  //       const errorData = await response.json();
-  //       console.error(errorData.error);
-  //     } else {
-  //       console.log("Block updated successfully");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   }
-  // };
-  // test2();
+  const {blocks} = useBlocksContext();  
 
   return (
     <div className="flex flex-col sm:flex-row sm:flex-wrap gap-4">
@@ -120,7 +32,7 @@ export default function Home() {
       <div className="flex flex-col gap-2 shadow-xl py-4 px-2 rounded-lg">
       {blocks.slice(1,5).map(
         (block:Block) => {
-          return <LinkBlock key={block.id} href={`/blocks/${block.id}`} Icon={VscArrowRight} IconSize={25}>{block.name}</LinkBlock>
+          return <LinkBlock key={block.id} href={`/home/blocks/${block.id}`} Icon={VscArrowRight} IconSize={25}>{block.name}</LinkBlock>
         }
       )}
         </div>
