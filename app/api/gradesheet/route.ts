17f4d1a2d94b78ecbe169a06e3dbb5230d7e9d6d
John Dao <104119657+johnd959@users.noreply.github.com>
2024-02-25 22:33:59 -0700

    Moved labpage and grading page, started UI work on grading page

---
+++ app/api/gradesheet/route.ts
@@ -0,0 +1,13 @@
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
+import { NextRequest } from "next/server";
+
+export async function POST(request:NextRequest){
+
+    const session = await getServerSession(authOptions); 
+
+    if(session){
+        
+    }
+
+}
--- app/home/blocks/[id]/page.tsx
+++ app/home/blocks/[id]/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 import React, { EffectCallback, useEffect } from "react";
-import { useState } from "react";
 import axios from "axios";
 import { Block } from "@/interfaces/block";
 
@@ -11,22 +11,10 @@ interface Props{
 
 export default function BlockPage({params}:Props){
 
-    const [block, setBlock]:[Block | undefined, Function] = useState(); 
-    const getBlock = async () => {
-        const data = await axios.get(`${process.env.APPLICATION_DOMAIN_URL}/api/blocks/${params.id}`);
-        return data.data
-    }
-    useEffect(
-        async () => {
-            let block:Block = await getBlock(); 
-            setBlock(block); 
-        }
-    )
-
 
     return(
         <section>
-            {}
+            {params.id}
         </section>
     )
 }
