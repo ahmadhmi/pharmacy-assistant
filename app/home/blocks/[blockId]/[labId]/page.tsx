5979d3ce5571fc8b8e3dfb1419232833689b023c
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2024-03-07 09:56:35 -0700

    Update page.tsx

--- app/home/blocks/[blockId]/[labId]/page.tsx
+++ app/home/blocks/[blockId]/[labId]/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { LabData } from "@/types/LabData";
 import React from "react";
 import MyButton from "@/app/UI/componenttest";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { group } from "console";
 const Data: LabData[] = [
   {
     student: "Qiaomu Lei 1",
@@ -36,8 +39,60 @@ const Data: LabData[] = [
     RxNum: ["123123123"],
   },
 ];
+interface Props {
+  params: {
+    blockId: string;
+    labId: string;
+  };
+}
+interface gradeSheet {
+  comment: string;
+  criteria: object;
+  date: Date;
+  labId: string;
+  rx: string;
+  studentID: string;
+  studentName: string;
+  _id: string;
+}
 
-export default function LabPage() {
+export default function LabPage({ params }: Props) {
+  const [gradeSheets, setGradeSheets] = useState([]);
+  // const [labData, setLabData] = useState<
+  //   Record<string, gradeSheet[]> | undefined
+  // >(undefined);
+  async function fetchGradingSheets() {
+    const data =
+      // await axios.get(`/api/blocks/${params.blockId}/${params.labId}/grading`)
+      (
+        await axios.get(
+          `/api/blocks/65cbe1af929966312830eea0/65e6c4517fdb6a053a93de8a/grading`
+        )
+      ).data;
+    setGradeSheets(data);
+    console.log(data);
+  }
+  useEffect(() => {
+    fetchGradingSheets();
+  }, []);
+
+  if (gradeSheets) {
+    const result = gradeSheets.reduce(
+      (
+        groupedGradeSheets: Record<string, gradeSheet[]>,
+        gradeSheet: gradeSheet
+      ) => {
+        const studentID = gradeSheet.studentID;
+        if (groupedGradeSheets[studentID] == null)
+          groupedGradeSheets[studentID] = [];
+        groupedGradeSheets[studentID].push(gradeSheet);
+        return groupedGradeSheets;
+      },
+      {}
+    );
+    console.log(result);
+  }
+
   return (
     <section className="display-flex justify-center h-screen w-100% px-8 py-10">
       <h1 className="text-center mb-6 text-3xl">Lab Page</h1>
@@ -50,16 +105,17 @@ export default function LabPage() {
             </div>
             <div className="collapse-content bg-primary">
               {item.RxNum.map((items, index) => (
-                <div className="flex flex-row items-center justify-between text-black my-2 ">
+                <div
+                  className="flex flex-row items-center justify-between text-black my-2 "
+                  key={index}
+                >
                   <input
                     title="checkbox"
                     type="checkbox"
                     defaultChecked
                     className=" checkbox border-black"
                   />
-                  <p key={index} className="font-bold">
-                    {items}
-                  </p>
+                  <p className="font-bold">{items}</p>
                   <div>
                     <button className="btn btn-sm">Edit</button>
                   </div>
