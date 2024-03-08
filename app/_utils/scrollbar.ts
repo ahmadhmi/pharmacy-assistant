182b2316747b0695a618eac2f1ae34de89ffc302
John Dao <104119657+johnd959@users.noreply.github.com>
2024-03-08 00:25:24 -0700

    Added some UI refinements and added gradesheet list to /grading page

---
+++ app/_utils/scrollbar.ts
@@ -0,0 +1,26 @@
+
+export function ScrollBar({addUtilities}:{addUtilities:Function}){
+    const newUtilities = {
+        ".scrollbar-none::-webkit-scrollbar":{
+            display: "none",
+        },
+        ".scrollbar-none": {
+            "-ms-overflow-style":"none",
+            "scrollbar-width":"none"
+        },
+        ".scrollbar-thin::-webkit-scrollbar":{
+            width: 6,
+        },
+        ".scrollbar-track::-webkit-scrollbar-track": {
+            "border-width": 1,
+            "border-color": "#000",
+            "border-style": "solid",
+            "border-radius": 10
+        },
+        ".scrollbar-thumb-black::-webkit-scrollbar-thumb": {
+            background: "#555",
+            "border-radius": 10,
+        }
+    };
+    addUtilities(newUtilities)
+}
