87a6d1a0f2d52fb243edab09f37ebac699595ea5
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2024-03-12 16:12:02 -0600

    update next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
 module.exports = {
-    images: {
-      domains: ['avatars.githubusercontent.com'],
-    },
-  }
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
+};
