5c732538293ab999db0c0bad274b1e254171ea48
John Dao <104119657+johnd959@users.noreply.github.com>
2024-04-12 14:42:56 -0600

    Left sth out of prev'

--- app/home/blocks/[blockId]/[weekId]/[labId]/grading/[gradesheetId]/page.tsx
+++ app/home/blocks/[blockId]/[weekId]/[labId]/grading/[gradesheetId]/page.tsx
@@ -454,7 +454,7 @@ export default function Grade({ params }: Props) {
                             }}
                         >
                             <div className="p-4 absolute right-3 bottom-5 bg-error mb-6 rounded-2xl text-neutral hover:alert-warning">
-                                <span>{`${error}`}</span>
+                                <p className="break-words">{`${error}`}</p>
                             </div>
                         </div>
                     ) : (
